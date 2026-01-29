531d4036a557cff6b8fad6e77a4043ae5b73a51a
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-29 20:16:59 -0300

    Add PileSettlement class with elastic_load method

---
+++ core/deep/pile_settlement.py
@@ -0,0 +1,9 @@
+# =====================================================
+# core/deep/pile_settlement.py
+# =====================================================
+
+
+class PileSettlement:
+@staticmethod
+def elastic_load(P, L, A, Ep):
+return (P * L) / (A * Ep)
