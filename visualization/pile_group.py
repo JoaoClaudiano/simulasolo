5274a2067cfe77522392291b230e34bc89d62bda
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-29 20:32:28 -0300

    Create pile_group.py

---
+++ visualization/pile_group.py
@@ -0,0 +1,14 @@
+# =====================================================
+# visualization/pile_group.py
+# =====================================================
+
+
+class PileGroupPlot:
+@staticmethod
+def efficiency(n_piles, efficiency):
+plt.figure()
+plt.bar(n_piles, efficiency)
+plt.xlabel("Número de Estacas")
+plt.ylabel("Eficiência do Grupo")
+plt.title("Eficiência de Grupo de Estacas")
+BasePlot.show()
