79e20e4feb1c816472618389101b990d5a633384
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-29 20:03:28 -0300

    Create group.py

---
+++ core/deep/group.py
@@ -0,0 +1,14 @@
+# =====================================================
+# core/deep/group.py
+# =====================================================
+
+
+class PileGroup:
+@staticmethod
+def efficiency(n, spacing, diameter):
+return min(1.0, 1 - 0.1 * (diameter / spacing) * (n - 1))
+
+
+@staticmethod
+def group_capacity(single_capacity, n, efficiency):
+return single_capacity * n * efficiency
