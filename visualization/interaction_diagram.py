1752d6ea6da8ef00aaa470925a63d2de2b4eb449
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-29 20:31:25 -0300

    Create interaction_diagram.py

---
+++ visualization/interaction_diagram.py
@@ -0,0 +1,14 @@
+# =====================================================
+# visualization/interaction_diagram.py
+# =====================================================
+
+
+class InteractionDiagram:
+@staticmethod
+def plot(N, M):
+plt.figure()
+plt.plot(N, M)
+plt.xlabel("Força Normal")
+plt.ylabel("Momento")
+plt.title("Diagrama de Interação N–M")
+BasePlot.show()
