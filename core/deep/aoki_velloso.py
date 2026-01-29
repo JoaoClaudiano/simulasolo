36039f5d8c32865d5fee7c8cf2aef1016835314c
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-29 20:03:05 -0300

    Add AokiVelloso class with capacity method

---
+++ core/deep/aoki_velloso.py
@@ -0,0 +1,13 @@
+# =====================================================
+# core/deep/aoki_velloso.py
+# =====================================================
+
+
+class AokiVelloso:
+@staticmethod
+def capacity(spt, pile: Pile):
+qs = 0.02 * spt
+qb = 0.1 * spt
+side = math.pi * pile.diameter * pile.length * qs
+base = math.pi * (pile.diameter/2)**2 * qb
+return side + base
