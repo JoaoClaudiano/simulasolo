7c15c44c479f4ee7c4a23e78d16300e757e66d43
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-29 20:15:40 -0300

    Add SandSettlement class with elastic method

---
+++ core/settlement/sand.py
@@ -0,0 +1,9 @@
+# =====================================================
+# core/settlement/sand.py
+# =====================================================
+
+
+class SandSettlement:
+@staticmethod
+def elastic(q, B, Es):
+return q * B / Es
